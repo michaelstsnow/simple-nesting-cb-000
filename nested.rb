209e197b956e4e746c0ec4be9db5ec6a97091d1d
michaelstsnow <michael.st.snow@gmail.com>
2018-11-07 13:08:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,10 +79,8 @@ def adding_matz
         }
      }
 		 #programmer_hash[:yukihiro_matsumoto]={}
-		 programmer_hash[:yukihiro_matsumoto]={
-			 :known_for => "Ruby",
-			 :languages => ["LISP","C"]
-		 }
+		 programmer_hash[:yukihiro_matsumoto]={}
+		 programmer_hash
 		 #programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
 end
 
